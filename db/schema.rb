1ba3055ace7551f426d2143b6017b11ab17b9cc3
MoranneBleuzen <moranne.bleuzen@kedgebs.com>
2019-05-20 18:32:52 +0200

    model wagoners done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_145730) do
+ActiveRecord::Schema.define(version: 2019_05_20_161437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,16 @@ ActiveRecord::Schema.define(version: 2019_05_20_145730) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wagoners", force: :cascade do |t|
+    t.text "description"
+    t.string "github_name"
+    t.integer "rating"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wagoners_on_user_id"
+  end
+
+  add_foreign_key "wagoners", "users"
 end
