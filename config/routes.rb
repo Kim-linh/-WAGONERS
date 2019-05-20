7a32435f816c724f53df1f329b46b6b8fc67bbc0
maeva pham <maevapham.contact@gmail.com>
2019-05-20 17:34:56 +0200

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'wagoners#index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :wagoners do
+    resources :bookings, only: [ :new, :create, :show]
+  end
+  resources :users, only: [ :show]
 end
