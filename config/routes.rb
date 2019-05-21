05fdf8ac37740742b8d86c91b6fb2c7cead60d51
MoranneBleuzen <moranne.bleuzen@kedgebs.com>
2019-05-21 16:14:44 +0200

    controller booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'bookings/start_at:datetime'
+  get 'bookings/end_at:datetime'
+  get 'bookings/user:references'
+  get 'bookings/wagoner:references'
   root to: 'wagoners#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
