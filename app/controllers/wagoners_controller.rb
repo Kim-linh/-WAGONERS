1c090785c656f0f20feab47dad0b686be253d0ba
maeva pham <maevapham.contact@gmail.com>
2019-05-22 13:51:36 +0200

    bugs fixés par Sarah, merci

--- app/controllers/wagoners_controller.rb
+++ app/controllers/wagoners_controller.rb
@@ -1,45 +1,47 @@
 class WagonersController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :find_wagoner, only: [:show, :edit, :update, :delete]
 
   def index
     @wagoners = Wagoner.all
   end
+
+
+  def show
+  end
+
+  def new
+    @wagoner = Wagoner.new
+  end
+
+  def create
+    @wagoner = Wagoner.new(wagoner_params)
+    if @wagoner.save
+      redirect_to wagoners_path(@wagoner)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @wagoner.update(wagoner)
+  end
+
+  def delete
+    @wagoner.destroy
+  end
+
+  private
+
+  def find_wagoner
+    @wagoner = Wagoner.find(params[:id])
+  end
+
+  def wagoner_params
+    params.require(:wagoner).permit(:description, :github_name, :price)
+  end
 end
 
-#   def show
-#     @wagoner = Wagoner.find(params[:id])
-#   end
-
-#   def new
-#     @wagoner = Wagoner.new
-#   end
-
-#   def create
-#     @wagoner = Wagoner.new(wagoner_params)
-#     if @wagoner.save
-#       redirect_to wagoners_path(@wagoner)
-#     else
-#       render :new
-#     end
-#   end
-
-#   def edit
-#     @wagoner = Wagoner.find(params[:id])
-#   end
-
-#   def update
-#     @wagoner = Wagoner.find(params[:id])
-#     @wagoner.update(wagoner)
-#   end
-
-#   def delete
-#     @wagoner = Wagoner.find(params[:id])
-#     @wagoner.destroy
-#   end
-
-#   private
-
-#   def wagoner_params
-#     params.require(:wagoner).permit(:description, :github_name, :price)
-#   end
-# end
