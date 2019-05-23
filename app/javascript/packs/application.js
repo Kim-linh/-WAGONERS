cd12d3d66ec273a000582d88754b3cb19fdb25f5
Kurt Pascual <kurt.pascual84@gmail.com>
2019-05-23 14:19:40 +0200

    init marker map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 
+
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from './init_mapbox';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 initMapbox();
