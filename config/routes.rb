ea4cfa0b1888392dde1e8d1eb41add477508c864
maeva pham <maevapham.contact@gmail.com>
2019-05-23 17:37:09 +0200

    creation de la show pour le Wagoner

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :wagoners do
+    member do                             # member => restaurant id in URL
+      get 'wag_show'                          # RestaurantsController#chef
+    end
     resources :bookings, only: [ :index, :new, :create]
   end
   # resources :users, only: [ :show]
