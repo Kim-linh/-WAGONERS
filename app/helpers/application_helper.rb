18fa15a0286a02f0170e17e4fda7e808689afdc0
maeva pham <maevapham.contact@gmail.com>
2019-05-24 13:29:40 +0200

    changement de la couleur des etoiles du rating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def display_stars(stars_number)
-   ("<i class='fas fa-star'></i>" * stars_number + "<i class='far fa-star'></i>" * (5 - stars_number)).html_safe if !stars_number.nil?
+   ("<i id='stars' class='fas fa-star'></i>" * stars_number + "<i class='far fa-star'></i>" * (5 - stars_number)).html_safe if !stars_number.nil?
   end
 end
