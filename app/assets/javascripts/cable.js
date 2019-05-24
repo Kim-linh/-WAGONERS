0732080db097a3d244f0963cdda5a1b48f2c61ff
MoranneBleuzen <moranne.bleuzen@kedgebs.com>
2019-05-24 12:42:11 +0200

    debegging ok

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,4 +1,4 @@
-touch app/javascript/plugins/init_autocomplete.js// Action Cable provides the framework to deal with WebSockets in Rails.
+// Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 //
 //= require action_cable
